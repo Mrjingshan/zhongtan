deaa897c52db6a929b4111eb653fd56eb9a54f95
qijingshan <2578379684@qq.com>
2019-07-08 10:04:12 +0800

    first-commit

---
+++ mock/user.js
@@ -0,0 +1,84 @@
+
+const tokens = {
+  admin: {
+    token: 'admin-token'
+  },
+  editor: {
+    token: 'editor-token'
+  }
+}
+
+const users = {
+  'admin-token': {
+    roles: ['admin'],
+    introduction: 'I am a super administrator',
+    avatar: 'https://imgsa.baidu.com/forum/w%3D580%3B/sign=dc0e245c99dda144da096cba828cd2a2/ae51f3deb48f8c544437456034292df5e1fe7f9d.jpg',
+    name: 'Super Admin'
+  },
+  'editor-token': {
+    roles: ['editor'],
+    introduction: 'I am an editor',
+    avatar: 'https://imgsa.baidu.com/forum/w%3D580%3B/sign=458a434af903918fd7d13dc261062797/d52a2834349b033b7d18e8701bce36d3d439bd83.jpg',
+    name: 'Normal Editor'
+  }
+}
+
+export default [
+  // user login
+  {
+    url: '/user/login',
+    type: 'post',
+    response: config => {
+      const { username } = config.body
+      const token = tokens[username]
+
+      // mock error
+      if (!token) {
+        return {
+          code: 60204,
+          message: 'Account and password are incorrect.'
+        }
+      }
+
+      return {
+        code: 20000,
+        data: token
+      }
+    }
+  },
+
+  // get user info
+  {
+    url: '/user/info\.*',
+    type: 'get',
+    response: config => {
+      const { token } = config.query
+      const info = users[token]
+
+      // mock error
+      if (!info) {
+        return {
+          code: 50008,
+          message: 'Login failed, unable to get user details.'
+        }
+      }
+
+      return {
+        code: 20000,
+        data: info
+      }
+    }
+  },
+
+  // user logout
+  {
+    url: '/user/logout',
+    type: 'post',
+    response: _ => {
+      return {
+        code: 20000,
+        data: 'success'
+      }
+    }
+  }
+]
---
+++ src/store/modules/permission.js
@@ -0,0 +1,89 @@
+// import { asyncRoutes, constantRoutes } from '@/router'
+import { getRoute } from '@/api/user'
+import { constantRoutes } from '@/router'
+import Layout from '@/layout'
+const _import = require('@/utils/_import_' + process.env.NODE_ENV)//获取组件的方法
+/**
+ * Use meta.role to determine if the current user has permission
+ * @param roles
+ * @param route
+ */
+
+function hasPermission(roles, route) {
+  // 判断权限是否显示
+  if (route.meta && route.meta.roles) {
+    return roles.some(role => route.meta.roles.includes(role))
+  } else {
+    return true
+  }
+}
+
+/**
+ * Filter asynchronous routing tables by recursion
+ * @param routes asyncRoutes
+ * @param roles
+ */
+function filterAsyncRouter(asyncRouterMap) { //遍历后台传来的路由字符串，转换为组件对象
+  const accessedRouters = asyncRouterMap.filter(route => {
+    if (route.component) {
+      switch (route.component) {
+        case 'Layout':
+          route.component = Layout
+          break;
+        case 'render':
+          route.component = { render(c) { return c("router-view") }}
+          break;
+        default:
+          route.component = _import(route.component)
+          break;
+      }
+    }
+    if (route.children && route.children.length) {
+      route.children = filterAsyncRouter(route.children)
+    }
+    return true
+  })
+  return accessedRouters
+}
+const state = {
+  routes: [],
+  addRoutes: []
+}
+
+const mutations = {
+  SET_ROUTES: (state, routes) => {
+    state.addRoutes = routes
+    state.routes = constantRoutes.concat(routes)
+  }
+}
+
+const actions = {
+  generateRoutes({ commit }, roles) {
+    var url;
+    if (roles[0]==="admin") {
+      url = 'https://www.easy-mock.com/mock/5b69b69ef770e33d7c1342a5/example/123456'
+    }else{
+      url = 'https://www.easy-mock.com/mock/5b69b69ef770e33d7c1342a5/example/qijingshanrouter'
+    }
+    return new Promise(resolve => {
+      getRoute(url).then(res=>{
+        let accessedRoutes 
+        // 根据角色生成对应权限的路由
+        accessedRoutes =  filterAsyncRouter(res.data.route)
+        commit('SET_ROUTES', res.data.route)
+        resolve(accessedRoutes)
+      }).catch(error=>{
+        console.log(error);
+      });
+
+     
+    })
+  }
+}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions
+}
